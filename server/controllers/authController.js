a84259b907c4a9b262234c0b2dbdfc40185bbf7e
danielufeli <danielufeli@yahoo.com>
2019-07-02 01:10:30 +0100

    Added signin endpoint
    Developed user signin endpoint
    Validated user input
    wrote more test for signin endpoint and refactored other test
    
    finishes #166960352

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,27 +1,27 @@
 import dotenv from 'dotenv';
-import authtok from '../helpers/authok';
 import userObjects from '../helpers/userObjects';
 
-const { newUser } = userObjects;
+const { newUser, generateUserToken, userByEmail } = userObjects;
 dotenv.config();
 
 class authController {
   static async userSignup(req, res) {
-    const hash = authtok.hashPassword(req.body.password);
     try {
-      const user = await newUser(hash, req);
-      const {
-        id, isadmin, firstname, lastname, email,
-      } = user;
-      const userToken = authtok.generateToken(id, isadmin, email, firstname, lastname);
-      res.status(201).json({
-        status: 'success',
-        data: {
-          id, isadmin, token: userToken,
-        },
-      });
+      const user = await newUser(req);
+      const data = generateUserToken(user);
+      res.status(201).json({ status: 'success', data });
     } catch (error) {
       res.status(500).json(error);
+    }
+  }
+
+  static async userSignin(req, res) {
+    try {
+      const user = await userByEmail(req.body.email);
+      const data = generateUserToken(user);
+      return res.status(200).json({ status: 'success', data });
+    } catch (error) {
+      return res.status(500).json(error);
     }
   }
 }
--- server/helpers/userObjects.js
+++ server/helpers/userObjects.js
@@ -1,10 +1,12 @@
 import userModel from '../models/userModel';
 import db from '../db';
+import authtok from './authok';
 
 const { createUser, allUser } = userModel;
 
 export default class userObjects {
-  static async newUser(hash, req) {
+  static async newUser(req) {
+    const hash = authtok.hashPassword(req.body.password);
     const values = [
       req.body.email,
       req.body.mobileno,
@@ -24,13 +26,22 @@ export default class userObjects {
 
   static async userByEmail(uemail) {
     const users = await userObjects.allUsers();
-    const currentUser = users.find(v => v.email === uemail);
+    const currentUser = users.find(user => user.email === uemail);
     return currentUser;
   }
 
   static async userByMobile(umobile) {
     const users = await userObjects.allUsers();
-    const currentUser = users.find(v => v.mobileno === umobile);
+    const currentUser = users.find(user => user.mobileno === umobile);
     return currentUser;
+  }
+
+  static generateUserToken(user) {
+    const {
+      id, firstname, lastname, email, isadmin,
+    } = user;
+    const userToken = authtok.generateToken(id, isadmin, email, firstname, lastname);
+    const data = { id, isadmin, token: userToken };
+    return data;
   }
 }
