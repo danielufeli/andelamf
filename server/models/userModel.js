92ecd461309512155cfffa4c3b489d799e06ba8d
danielufeli <danielufeli@yahoo.com>
2019-07-14 23:48:31 +0100

    Changes for Submission

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,6 @@
 const userModel = {
   createUser: `INSERT INTO 
-    users(first_name, last_name, password, email)
+    users(email, password, first_name, last_name)
     values($1, $2, $3, $4)
     returning *`,
   allUser: 'SELECT * FROM users',
