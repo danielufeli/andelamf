1f05e3ccccc2bd793292fb4a3740d52f4e7631ab
danielufeli <danielufeli@yahoo.com>
2019-07-15 07:56:10 +0100

    Update for submittions

--- server/helpers/validateTrip.js
+++ server/helpers/validateTrip.js
@@ -1,7 +1,6 @@
 import Joi from '@hapi/joi';
 
 const validateTrip = {
-  bus_id: Joi.number().min(1).required(),
   origin: Joi.string().required(),
   destination: Joi.string().required(),
   fare: Joi.number().required(),
