458af1e3075062614909e1991fb50ea84663fdfa
danielufeli <danielufeli@yahoo.com>
2019-07-15 09:10:10 +0100

    update for submittion

--- server/helpers/validateBooking.js
+++ server/helpers/validateBooking.js
@@ -2,8 +2,6 @@ import Joi from '@hapi/joi';
 
 const validateBooking = {
   trip_id: Joi.number().min(1).required(),
-  user_id: Joi.number().min(1).required(),
-  seat_number: Joi.number().min(1).required(),
 };
 
 export default validateBooking;
