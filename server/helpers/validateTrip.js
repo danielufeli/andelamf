7d4067dbeb1a641a508a47476a86170df4894e72
danielufeli <danielufeli@yahoo.com>
2019-07-15 07:52:26 +0100

    Update for submittions

--- server/helpers/validateTrip.js
+++ server/helpers/validateTrip.js
@@ -2,11 +2,9 @@ import Joi from '@hapi/joi';
 
 const validateTrip = {
   bus_id: Joi.number().min(1).required(),
-  origin: Joi.string().min(2).max(50).required(),
-  destination: Joi.string().min(2).max(255).required(),
-  trip_date: Joi.date().required()
-    .error(() => ({ message: 'Trip date is invalid, enter date in this formt(YYYY-MM-DD)' })),
-  fare: Joi.number().min(2).required(),
+  origin: Joi.string().required(),
+  destination: Joi.string().required(),
+  fare: Joi.number().required(),
 };
 
 export default validateTrip;
