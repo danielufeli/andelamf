ac348fc6c352fc0fb551530ca41677fc8de97133
danielufeli <danielufeli@yahoo.com>
2019-07-23 03:49:16 +0100

    Added feature for user to be able to get and filter trips by destination
    - Added filter for destination in the trip route
    
    Finishes: #167429805

--- server/middleware/checkTrip.js
+++ server/middleware/checkTrip.js
@@ -1,14 +1,22 @@
 import dataObjects from '../helpers/dataObjects';
 import tripModel from '../models/tripModel';
 
-const { allTrips, getTripById } = tripModel;
-const { getAllData, getTripBooking, getBusTrip } = dataObjects;
+const { allTrips } = tripModel;
+const {
+  getAllData, getTripBooking, getBusTrip, getTripParams, filterDestination,
+} = dataObjects;
 
 export default class checkTrips {
   static async checkAllTrips(req, res, next) {
     try {
       const trips = await getAllData(allTrips);
       if (trips.length < 1) { return res.status(404).json({ status: 'error', error: 'No Trips Available' }); }
+      const { destination } = req.query;
+      if (destination !== undefined) {
+        const data = filterDestination(trips, destination);
+        if (data.length < 1) { return res.status(404).json({ status: 'error', error: 'No Trips Found' }); }
+        return res.status(200).json({ status: 200, data });
+      }
       return next();
     } catch (error) { return next(error); }
   }
@@ -17,6 +25,7 @@ export default class checkTrips {
     try {
       const trip = await getTripBooking(req);
       if (!trip) { return res.status(404).json({ status: 'error', error: 'Trip Not Available' }); }
+      if (trip.status === 'cancelled') { return res.status(400).json({ status: 'error', error: 'Trip has been cancelled' }); }
       return next();
     } catch (error) { return next(error); }
   }
@@ -31,10 +40,9 @@ export default class checkTrips {
 
   static async checkTripUpdate(req, res, next) {
     try {
-      const { tripId } = req.params;
-      const trips = await getAllData(allTrips);
-      const trip = trips.find(userTrip => userTrip.trip_id === Number(tripId));
-      if (!trip) return res.status(404).json({ status: 'error', error: 'No Trip Available' });
+      const trip = await getTripParams(req);
+      if (!trip) { return res.status(404).json({ status: 'error', error: 'No Trip Available' }); }
+      if (trip.status === 'cancelled') { return res.status(400).json({ status: 'error', error: 'Trip has been cancelled' }); }
       return next();
     } catch (error) { return next(error); }
   }
